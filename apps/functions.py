0cd79d2e38759b6d9d103dcadddafa2610c1a3de
doolachen <919550793@qq.com>
2022-12-21 16:51:56 +0800

    灰度翻转

--- apps/functions.py
+++ apps/functions.py
@@ -1,8 +1,16 @@
+import copy
+
 import streamlit as st
 from utils.reader import Reader
 from utils.mapping import mappingByWindow
+from utils.grayReverse import grayReverse
 
+current_img = None
+initFlag = False
 def app():
+    global initFlag
+    global current_img
+
     st.set_option('deprecation.showfileUploaderEncoding', False)
     st.header("医用X光透视图像处理程序")
     # Upload tif image for processing
@@ -13,8 +21,6 @@ def app():
         img = Reader()
         img.readfrompath(path=img_path)
 
-        ori_img = img.data
-
         st.sidebar.header('图像基本信息')
         info = '图像宽：{}<br>图像高：{}<br>数据类型：{}<br>最小像素值：{}<br>最大像素值：{}'.format(img.h, img.w, img.type,
                                                                                                 img.minpixel,
@@ -23,22 +29,32 @@ def app():
         default_wl = (img.maxpixel - img.minpixel)//2
         default_ww = img.maxpixel - img.minpixel
 
-        resetwlww = st.sidebar.button('恢复默认图像')
-        slider_wl = st.sidebar.slider('窗位调节', min_value=0, max_value=4095, value=int(default_wl))
-        slider_ww = st.sidebar.slider('窗宽调节', min_value=0, max_value=4095,value=int(default_ww))
+        def reset():
+            st.session_state.sliderwl = default_wl
+            st.session_state.sliderww = default_ww
 
-        col1, col2, col3 = st.columns([1, 2, 4])
+        resetwlww = st.sidebar.button('恢复默认窗宽窗位',on_click=reset)
+        st.sidebar.write('默认窗位：', default_wl, '默认窗宽：', default_ww)
+
+        slider_wl = st.sidebar.slider('窗位调节', min_value=0, max_value=4095, value=int(default_wl),key='sliderwl')
+        slider_ww = st.sidebar.slider('窗宽调节', min_value=0, max_value=4095,value=int(default_ww),key='sliderww')
+
+        if initFlag is not True:
+            current_img = mappingByWindow(img.data, slider_wl, slider_ww)
+            initFlag = True
+
+        col1, col2= st.columns([1, 4])
         with col1:
-            bt_left = st.button('灰度反转')
+            bt_grayReverse = st.button('灰度反转')
         with col2:
-            bt_right = st.button('左右翻转')
-        with col3:
-            bt_reset = st.button('恢复图像')
+            bt_LFflip = st.button('左右翻转')
 
-        if resetwlww:
-            slider_wl = default_wl
-            slider_ww = default_ww
+        if bt_grayReverse:
+            current_img = grayReverse(current_img)
+        else:
+            current_img = mappingByWindow(img.data, slider_wl, slider_ww)
 
-        current_img = mappingByWindow(img.data, slider_wl, slider_ww)
 
         st.image(current_img, '当前图像')          # 需要用一个current image来保存 每次都对current图像处理
+
+
--- utils/mapping.py
+++ utils/mapping.py
@@ -1,13 +1,14 @@
 import numpy as np
 import copy
-# 映射到 0-255
-#
 
-def mappingByWindow(data:np.ndarray,wl:int,ww:int) -> np.ndarray:
+
+# 映射到 0-255
+
+def mappingByWindow(data: np.ndarray, wl: int, ww: int) -> np.ndarray:
     out = copy.deepcopy(data)
     left = wl - ww // 2
     right = wl + ww // 2
-    out[out>right] = right
-    out[out<left] = left
-    out = ((out-left)/ww * 255).astype('uint8')
+    out[out > right] = right
+    out[out < left] = left
+    out = ((out - left) / ww * 255).astype('uint8')
     return out
